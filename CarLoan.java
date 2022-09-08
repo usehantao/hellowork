a863e977b295636ffc8bdd69644462c83b733e43
DESKTOP-D1V0F9J\hp <iverson910919@gmail.com>
2022-09-08 23:01:18 +0800

    2022.9.8

---
+++ CarLoan.java
@@ -0,0 +1,31 @@
+//a progrm that calculates a  car payment 
+
+public class CarLoan{
+	public static void main(String[]args) {
+		int carLoan = 10000;
+		  int loanLength =3;
+		  int interestRate =5;
+		  int downPayment =2000;
+		  
+		  if(loanLength<=0 ||interestRate<=0) {
+			  System.out.println("Error! You must take out a valid car loan.");
+			  
+			  
+		  }
+		  else if(downPayment>=carLoan) {
+			  System.out.println("The car can be paid in full");
+			  
+		  }
+		  
+		  else {
+			   int remainingBalance = carLoan-downPayment;
+			   int months =  loanLength*12;
+			   int monthsBalance=remainingBalance / months;
+			    int interest=monthsBalance * interestRate/100;
+			    int monthlypPayment = monthsBalance +interest;
+			    		
+			    		System.out.println(monthlypPayment);
+			    
+		  }
+	}
+}
