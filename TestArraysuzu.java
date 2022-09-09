e72a56db7d1abea236b4813fed941ad3501daf40
DESKTOP-D1V0F9J\hp <iverson910919@gmail.com>
2022-09-09 15:08:24 +0800

    2022.9.9

---
+++ TestArraysuzu.java
@@ -0,0 +1,10 @@
+public class TestArraysuzu{
+	public static void main(String[]args) {
+		double[]myList= {1.9,2.9,3.4,3.5};
+		//打印所有数组
+		for(double element:myList) {
+			System.out.println(element);
+		}
+		
+	}
+}
