f26a160eeec35d1e3b1693fccd5015ccc139c610
https://github.com/usehantao/hellowork.git <iverson910919@gmail.com>
2023-10-15 21:07:18 +0800

    io流作业 序列与反序列化

--- Homework03.java
+++ Homework03.java
@@ -1,25 +1,64 @@
-public class Homework03{
+package com.jiheDome;
 
-		public static void main(String[]args){
-		String name = "Han Taos Pingg";
-		printName(name);
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.Properties;
+
+public class HomeWork03 {
+	public static void main(String[]args) throws Exception, Exception {
+		
+		Properties pr =new Properties();
+		pr.load(new FileReader("src/dog.properties"));
+		String name =pr.get("name")+"";
+		int age = Integer.parseInt(pr.get("age")+"");
+		String color =pr.get("color")+"";
+		
+		Dog dog = new Dog(name,age,color);
+		//System.out.print(dog);
+		
+		String serFilePath ="/Users/apple/Desktop/f1.dat";
+		ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(serFilePath));
+		os.writeObject(dog);
+		os.close();
+		//System.out.print("序列化完成");
+		
+		m1();
+		
+		
+	}
+	public static void m1() throws Exception{
+		String serFilepath ="/Users/apple/Desktop/f1.dat";
+		ObjectInputStream oos = new ObjectInputStream(new FileInputStream(serFilepath));
+		Dog dog =(Dog)oos.readObject();
+		oos.close();
+		System.out.print("反序列化后"+dog);
+		
+	}
+
+}
+class Dog implements Serializable{
+	private String name;
+	private int age;
+	private String color;
+	
+	public Dog(String name ,int age ,String color) {
+		this.name=name;
+		this.age=age;
+		this.color=color;
 		
 		
 	}
 
-	 public static void printName(String str){
-		if(str == null){
-		System.out.println("str 不能为空");
-		return;
-		}
-		//进行字符串分割
-		String [] names = str.split(" ");
-		if(names.length != 3){
-		System.out.println("输入字符串格式不对");
-		return;
-	}	
-		//进行格式填充
-		String format = String.format("%s,%s .%c" , names[2] ,names[0],names[1].toUpperCase().charAt(0));
-		System.out.println(format);
-		}
+	@Override
+	public String toString() {
+		return "Dog [name=" + name + ", age=" + age + ", color=" + color + "]";
 	}
+	
+	
+}
